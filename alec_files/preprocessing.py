7aa43e1836c3a4d7a28782304e53941d31500994
Alec Hartman <aleclhartman08@gmail.com>
2020-06-02 14:17:25 -0500

    created validate split

--- alec_files/preprocessing.py
+++ alec_files/preprocessing.py
@@ -16,7 +16,7 @@ from sklearn.model_selection import train_test_split
 import acquire as ac
 import prepare as pr
 
-def split_repo_data(df, test_size=.2, train_size=.8, random_state=56):
+def split_repo_data(df, test_size=.2, train_size=.8, validate_size=.75, random_state=56):
     """
     This function takes in a DataFrame and returns a train and test DataFrame for exploration and modeling.
     I would like to stratify by language, but we need to get more than one observation per language in order to do so.
@@ -25,7 +25,10 @@ def split_repo_data(df, test_size=.2, train_size=.8, random_state=56):
     TODO: Stratify or bin to remove stratification necessity
     """
     
-    # call train_test_split on the DataFrame
+    # call train_test_split on df
     train, test = train_test_split(df, test_size=test_size, train_size=train_size, random_state=random_state)
 
-    return train, test
+    # call train_test_split on the train
+    train, validate = train_test_split(train, train_size=validate_size, random_state=random_state)
+
+    return train, validate, test
